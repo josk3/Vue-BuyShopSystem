e3115ec5178e8fbab121fa45b63492b9d709d8df
Jian <jian.9long@gmail.com>
2020-08-24 05:04:43 +0800

    u

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,9 +1,8 @@
 import axios from 'axios'
-import {MessageBox, Message} from 'element-ui'
+import {Message, MessageBox} from 'element-ui'
 import store from '@/store'
 import {getToken, getTokenKey} from '@/service/auth/token'
 import configs from '@/configs'
-import user from "@/store/modules/user";
 
 // create an axios instance
 const service = axios.create({
