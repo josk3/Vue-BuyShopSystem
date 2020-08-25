b4a5afc180c817462c6d8e3f7add3ac2a32af182
Jian <jian.9long@gmail.com>
2020-08-25 11:39:08 +0800

    u

--- src/permission.js
+++ src/permission.js
@@ -56,11 +56,10 @@ router.beforeEach(async (to, from, next) => {
                     // set the replace: true, so the navigation will not leave a history record
                     next({...to, replace: true})
                 } catch (error) {
-                    // remove token and go to login page to re-login
-                    await store.dispatch('user/resetToken')
                     Message.error(error || 'Has Error')
-                    let redirectTo = renderRedirectTo(to.path)
-                    next(`/login${redirectTo}`)
+                    //await store.dispatch('user/resetToken')
+                    //let redirectTo = renderRedirectTo(to.path)
+                    //next(`/login${redirectTo}`)
                     NProgress.done()
                 }
             }
