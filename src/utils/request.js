ca91c1b9c70524cc76d562256688a99e6d457726
jian <jian.9long@gmail.com>
2020-10-16 13:39:11 +0800

    Update request.js

--- src/utils/request.js
+++ src/utils/request.js
@@ -17,7 +17,7 @@ import {getSplitLast, toLower} from "@/utils/strUtils";
 const service = axios.create({
     baseURL: process.env.VUE_APP_BASE_API, // url = base url + request url
     // withCredentials: true, // send cookies when cross-domain requests
-    timeout: 7000 // request timeout 7s
+    timeout: 15000 // request timeout 15s
 })
 
 // request interceptor
