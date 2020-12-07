93561fe485742ceb1311891d8b3a87ed017c5450
jian <jian.9long@gmail.com>
2020-12-07 15:34:53 +0800

    search support email

--- src/views/portal/orders/search.vue
+++ src/views/portal/orders/search.vue
@@ -162,7 +162,7 @@
                 loading: false,
                 searchParams: {
                     title: 'nav.trade_manage', page: 1,
-                    trade_id: '', merchant_order_no: '', pay_status: '', search_date: ''
+                    trade_id: '', merchant_order_no: '', pay_status: '', search_date: '', email: ''
                 },
                 tabData: {list: [], page: {count: 0, page_num: 0, total: 0}},
                 paneName: 'all', //默认
