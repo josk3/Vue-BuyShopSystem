427a6bec4af615c8408ea6031e2f3f1c447b8c86
qiuZhangMax <38774732+qiuzhangMax@users.noreply.github.com>
2021-10-13 11:35:21 +0800

    问题单批量上传

--- src/assets/lang/zh.js
+++ src/assets/lang/zh.js
@@ -407,6 +407,7 @@ module.exports = {
         "payout_total": "总计",
         "batch_id_detail": "批次详情",
         "sure": "确 定",
+        "batch_upload_reject":"批量上传问题单",
     },
     "order": {
         "trade_id_and_order_no": "流水号/订单号",
