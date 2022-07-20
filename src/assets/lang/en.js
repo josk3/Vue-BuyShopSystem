7484ffb27e5df7eecc7a2d942206ba0db9622b75
GH-GTR <2263406432@qq.com>
2022-07-20 15:33:13 +0800

    修改用户名的英文表达

--- src/assets/lang/en.js
+++ src/assets/lang/en.js
@@ -574,7 +574,7 @@ module.exports = {
         "remindClose": "Got it",
         "site_success": "Successful,The website will be approved within 1 to 3 hours on workingdays, and the result will be sent to you by email.",
         "mer_remark": "Please note",
-        "input_account": "For example: userName and password",
+        "input_account": "For example: username and password",
         "virtual_notes": "If you need permission to view the submitted website, please provide access information  (such as user name and password) to help our audit staff to review; If not, please ignore.",
     },
     "bank": {
